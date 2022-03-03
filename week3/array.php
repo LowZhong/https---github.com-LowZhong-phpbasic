3364ccb624dcdcec90cff9c07fe07f63212be1ca
LowZhong <lowzhong203056@dasein.edu.my>
2022-03-03 16:43:09 +0800

    array redo

--- week3/array.php
+++ week3/array.php
@@ -3,29 +3,55 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style/array.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .circle {
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            padding: 3px;
+            background: #fff;
+            border: 3px solid #000;
+            color: #000;
+            text-align: center;
+            font: 23px Arial, sans-serif;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="circle">
-        <?php
-        $number = array("34, 4, 5, -23, 45, 100");
+    <?php $array = array(41, 52, 33, 54, 65); ?>
+    <div class="container mt-5">
+        <div class="row text-center">
+            <?php for ($i = 0; $i <= count($array) - 1; $i++) { ?>
+                <div class="col">
+                    <div class="circle">
+                        <?php echo $array[$i]; ?>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
 
-        for ($i = 0; $i < count($number); $i++) {
-            echo $number[$i];
-        }
-        ?>
+    <?php
+    $array = array(41, 52, 33, 54, 65);
+    $temp = $array[0];
+    $array[0] = $array[4];
+    $array[4] = $temp;
+    ?>
 
-        <?php
-        $a = array(34, 4, 5, -23, 45, 100);
-            $x = array_reverse($a,true);
-            $y = array_reverse($a);
-            print_r($x);
-            echo "</br>";
-            print_r($y);
-        ?>
+    <div class="container mt-5">
+        <div class="row text-center">
+            <?php for ($i = 0; $i <= count($array) - 1; $i++) { ?>
+                <div class="col">
+                    <div class="circle">
+                        <?php echo $array[$i]; ?>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </div>
 </body>
 
