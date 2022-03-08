702ee9d0549c71d436aadf8bbb8b9463b86c1a3c
LowZhong <lowzhong203056@dasein.edu.my>
2022-03-09 00:06:56 +0800

    done

--- week4/calculatePerimeter.php
+++ week4/calculatePerimeter.php
@@ -9,10 +9,10 @@
 </head>
 
 <body>
-    <?php
-    $radius = 5;
-    echo "area of circle with radius = " . $radius . " is " . pi() * pow($radius, 2);
-    ?>
+<?php 
+  $radius =10;
+  $circumference= 2 * 3.14 * $radius;
+  echo "Circumference of Circle = ".$circumference;
+?>
 </body>
-
 </html>
