4a14e3372fbf9b9878defb14c03b46ba0257261c
LowZhong <lowzhong203056@dasein.edu.my>
2022-03-09 02:56:52 +0800

    done

---
+++ week4/formValidateAns.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>CalculateArea</title>
+</head>
+
+<body>
+    <?php
+    $radius = 10;
+    $circumference = 2 * 3.14 * $radius;
+    echo "Perimeter of Circle = " . $circumference;
+    ?>
+
+    <?php
+    $numberentered = $_GET['radius'];
+    if (is_numeric($numberentered)) {
+        echo "Perimeter of Circle = " . $circumference;
+    } else {
+        echo 'Please enter only numbers.';
+    }
+
+    ?>
+</body>
+
+</html>
