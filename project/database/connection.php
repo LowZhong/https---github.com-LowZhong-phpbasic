7cc15613f2d5c2a8d60814d16cb870a19a73ad90
LowZhong <lowzhong203056@dasein.edu.my>
2022-03-09 10:26:58 +0800

    CRUD create product

---
+++ project/database/connection.php
@@ -0,0 +1,19 @@
+<?php
+
+// used to connect to the database
+
+$host = "localhost";
+$db_name = "lowzhong2";
+$username = "lowzhong2";
+$password = "_r]h]qvpApYU!I4L";
+
+try {
+
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    echo "Connected successfully";
+}
+
+// show error
+catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
+}
