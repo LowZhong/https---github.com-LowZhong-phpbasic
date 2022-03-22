b70ce887f74e202b7a6a26af0b613fea116f76d3
LowZhong <lowzhong203056@dasein.edu.my>
2022-03-22 16:12:24 +0800

    create login page

---
+++ project/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!--container-->
+    <div class="container">
+        <div class="page-header">
+            <h1>Login Your Account</h1>
+        </div>
+
+
+        <form action="" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <input type='text' name='username' class='form-control' />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type='text' name='password' class='form-control' />
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' name='login' value='login' class='btn btn-primary' />
+
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+
+    </div>
+    <!--end container-->
+</body>
+
+</html>
