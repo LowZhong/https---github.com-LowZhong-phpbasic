77831998232e8d03c4238f23ddf96f1c3f9dbf3e
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-06 06:49:28 +0800

    order changes

--- project/order_create.php
+++ project/order_create.php
@@ -16,13 +16,12 @@
         <?php
         // include database connection
         include 'database/connection.php';
+        include 'database/function.php';
         // define variables and set to empty values
-        $orderID = $username = $orderTime = $product1 = $qty1 = $product2 = $qty2 = $product3 = $qty3 = "";
+        $orderID = $username = $product1 = $qty1 = $product2 = $qty2 = $product3 = $qty3 = "";
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-            include 'database/function.php';
             // posted values
-
             $username = $_POST['username'];
             $product1 = $_POST['product1'];
             $qty1 = $_POST['qty1'];
@@ -30,54 +29,64 @@
             $qty2 = $_POST['qty2'];
             $product3 = $_POST['product3'];
             $qty3 = $_POST['qty3'];
+            //$error['username'] = validateUsername($username);
+            //$error = array_filter($error);
 
-            function test_input($data)
-            {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
+            try {
+                // insert query
+                $query = "INSERT INTO 'order_summary' ('username') VALUES(?)";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                // bind the parameters
+                $stmt->bindParam(1, $username);
+                // Execute the query
+                if ($stmt->execute()) {
+                    $last_order_id = $con->lastInsertId();
+                    if ($last_order_id > 0) {
+                    }
+                }
+            } catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
             }
+            try {
+                $query = "INSERT INTO 'order_details' ('orderID', 'product', 'quantity') VALUES(?,?,?)";
 
+                //prepare query for execute
+                $stmt = $con->prepare($query);
+                //posted values
+                $stmt->bindParam(1, $last_order_id);
+                $stmt->bindParam(1, $product1);
+                $stmt->bindParam(1, $qty1);
+                //execute the query
+                if ($stmt->execute()) {
+                    $query = "INSERT INTO 'order_details' ('orderID', 'product', 'quantity') VALUES(?,?,?)";
 
-            $error['username'] = validateUsername($username);
-
-            $error = array_filter($error);
-            if (empty($error)) {
-
-                try {
-                    // insert query
-                    $query = "INSERT INTO order_summary ('username') VALUES(?)";
-                    // prepare query for execution
+                    //prepare query for execute
                     $stmt = $con->prepare($query);
-                    // bind the parameters
-                    //$stmt->bindParam(':orderID', $orderID);
-                    $stmt->bindParam(1, $username);
-                    /*$stmt->bindParam(':product1', $product1);
-                    $stmt->bindParam(':product2', $product2);
-                    $stmt->bindParam(':product3', $product3);
-                    $stmt->bindParam(':qty1', $qty1);
-                    $stmt->bindParam(':qty2', $qty2);
-                    $stmt->bindParam(':qty3', $qty3);*/
-
-                    // Execute the query
+                    //posted values
+                    $stmt->bindParam(1, $last_order_id);
+                    $stmt->bindParam(1, $product2);
+                    $stmt->bindParam(1, $qty2);
+                    //execute the query
                     if ($stmt->execute()) {
-                        $last_order_id = $con->lastInsertId();
-                        if ($last_order_id > 0) {
+                        $query = "INSERT INTO 'order_details' ('orderID', 'product', 'quantity') VALUES(?,?,?)";
+
+                        //prepare query for execute
+                        $stmt = $con->prepare($query);
+                        //posted values
+                        $stmt->bindParam(1, $last_order_id);
+                        $stmt->bindParam(1, $product3);
+                        $stmt->bindParam(1, $qty3);
+                        //execute the query
+                        if ($stmt->execute()) {
                         }
                     }
                 }
-
-                // show error
-                catch (PDOException $exception) {
-                    die('ERROR: ' . $exception->getMessage());
-                }
-            } else {
-                foreach ($error as $value) {
-                    echo "<div class='alert alert-danger'>$value <br/></div>"; //start print error msg
-                }
+            } catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
             }
         }
+
         ?>
 
         <!-- html form here where the product information will be entered -->
@@ -99,19 +108,19 @@
                             // execute our query
                             $stmt->execute();
                             echo '<tr>
-                                <td>Select Product '.$x.'</td>
+                                <td>Select Product ' . $x . '</td>
                                 <td>
                                 <div class="col">';
-                            echo "<select class='form_select' name='product".$x."' value='".$product1."'>";
+                            echo "<select class='form_select' name='product" . $x . "' value='" . $product1 . "'>";
                             echo '<option selected>Product ' . $x . '</option>';
                             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                                 extract($row);
-                                echo "<option value='". $id ."'>".$name."</option>";
+                                echo "<option value='" . $id . "'>" . $name . "</option>";
                             }
                             echo "</select>
                             </div>
                                 Quantity
-                                <input type='number' name='qty". $x ."' class='form-control' value='". $qty1 ."' />";
+                                <input type='number' name='qty" . $x . "' class='form-control' value='" . $qty1 . "' />";
                         }
                         // show error
                         catch (PDOException $exception) {
