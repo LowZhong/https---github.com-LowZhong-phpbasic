ff864b9315a6fd2a3d1566b451651a055b7afaa4
LowZhong <lowzhong203056@dasein.edu.my>
2022-04-05 14:53:38 +0800

    ceate order update

---
+++ project/order_update.php
@@ -0,0 +1,139 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Read Records - PHP CRUD Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled and minified Bootstrap CSS -->
+
+    <!-- custom css -->
+    <style>
+        .m-r-1em {
+            margin-right: 1em;
+        }
+
+        .m-b-1em {
+            margin-bottom: 1em;
+        }
+
+        .m-l-1em {
+            margin-left: 1em;
+        }
+
+        .mt0 {
+            margin-top: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Update Order</h1>
+        </div>
+        <!-- PHP read record by ID will be here -->
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        $orderDetailsID = isset($_GET['orderDetailsID']) ? $_GET['orderDetailsID'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'database/connection.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT product, quantity FROM order_details WHERE orderDetailsID = ? LIMIT 0,1";
+            $stmt = $con->prepare($query);
+
+            // this is the first question mark
+            $stmt->bindParam(1, $orderDetailsID);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $product = $_POST('product');
+            $product = $_POST('quantity');
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+
+        <!-- HTML form to update record will be here -->
+        <!-- PHP post to update record will be here -->
+        <?php
+        // check if form was submitted
+        if ($_POST) {
+            try {
+                // write update query
+                // in this case, it seemed like we have so many fields to pass and
+                // it is better to label them and not use question marks
+                $query = "UPDATE order_summary, order_details SET * WHERE id = :id";
+                // prepare query for excecution
+                $stmt = $con->prepare($query);
+
+                // posted values
+                $name = htmlspecialchars(strip_tags($_POST['name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                $price = htmlspecialchars(strip_tags($_POST['price']));
+
+                // bind the parameters
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':description', $description);
+                $stmt->bindParam(':price', $price);
+                $stmt->bindParam(':id', $id);
+                // Execute the query
+                if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was updated.</div>";
+                } else {
+                    echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                }
+            }
+            // show errors
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        } ?>
+
+
+
+        <!--we have our html form here where new record information can be updated-->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?orderDetailsID={$orderDetailsID}"); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Name</td>
+                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></textarea></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES);  ?>" class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save Changes' class='btn btn-primary' />
+                        <a href='order_summary.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+
+    </div>
+    <!-- end .container -->
+</body>
+
+</html>
